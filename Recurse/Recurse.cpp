f26a9b4ad451f56b81c45731d1fbcb503b65407c
tekater <120014627+tekater@users.noreply.github.com>
2023-08-31 00:13:36 +0900

    Update Recurse.cpp

--- Recurse/Recurse.cpp
+++ Recurse/Recurse.cpp
@@ -38,16 +38,56 @@ void elevator(int floor) {
     }
 }
 
+//int Factorial(int n, int i) {
+//    if (i == n) {
+//        return n;
+//    }
+//    return i * Factorial(n, i + 1);
+//}
+
+int Factorial(int n) {
+    if (n == 1) {
+        return n;
+    }
+    return n* Factorial(n - 1);
+}
+
+double Power(double a, int n) {
+    if (n == 1) {
+        return a;
+    }
+    return a * Power(a, n - 1);
+}
+
+int Fibonacci(int n)
+{
+    if (n < 2) {
+        return n;
+    }
+    return (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+
+        
+
 
 int main()
 {
     setlocale(0, "");
     //cout << "Hello world!";
-    int n;
+    int n; int i;
+    double d;
     cout << "Введите номер этажа\n";
     cin >> n;
 
     elevator(n);
 
+    cout << "\nВведите число для Факториала:\n"; cin >> n;
+    cout << "Факториал числа "  <<  n << ": " << Factorial(n) << endl; // Done
+
+    cout << "\nВведите число для Возведения в степень:\n"; cin >> d;
+    cout << "Введите Степень:\n"; cin >> i;
+    cout << d << " в степени " <<  i  << ": " << Power(d, i) << endl; // Done
+
+    cout << "Фибоначчи: " << Fibonacci(20) << endl; // Done? с void не додумал
 
 }
